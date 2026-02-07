6098669591954b3e5aabeb3696b9f9cc1be4e362
shengrui12 <shengrui1309@gmail.com>
2026-02-07 18:56:20 +0100

    Pygame.py

--- Pygame.py
+++ Pygame.py
@@ -1,6 +1,30 @@
+#IMPORTANT NOTES:
+
+#In the screen the minimum x and y position is 0 and the maximum is the size variable for x is -large and for y is -width. *
+#For example my size is (800, 500) and our square dimensions are 80, 80 
+#The minimum x position is 0 and the maximum is 720 (800 - 80)
+#The minimum y position is 0 and the maximum is 420 (500 - 80)
+# *We need to know that this isn't neccessary if you want the object to dissapear in the limits.
+
+#Import Pygame
 import pygame
 import sys
 pygame.init()
+
+#Def pantalla
+size = (800, 500)
+screen = pygame.display.set_mode(size)
+
+#Object Position
+cord_x = 400
+cord_y = 200
+
+#Movement x and y velocity
+speed_x = 3
+speed_y = 3
+
+#Def FPS(Frames per second)
+clock = pygame.time.Clock()
 
 #Def RGB 
 Black = (0, 0, 0)
@@ -9,24 +33,35 @@ Red = (255, 0, 0)
 Green = (0, 255, 0)
 Blue = (0, 0, 255)
 
-size = (800, 500)
-
-screen = pygame.display.set_mode(size)
-
+#Loop
 while True:
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       sys.exit()
 
+  #SCRIPT___Bounce___
+  
+  if (cord_x > 720 or cord_x < 0):
+    speed_x *= -1
+    #Inverts the x speed
+
+  if (cord_y > 420 or cord_y < 0):
+    speed_y *= -1
+    #Inverts the y speed
+  
+  #Movement
+  cord_x += speed_x 
+  cord_y += speed_y
+  
+  #Background color
   screen.fill(Black)
 
-  #SALA DE DIBUJO
-
-  pygame.draw.rect(screen, Red, (400, 200, 80, 80))
-  #pygame.draw.rect(surface, color, rect)
-  #pygame.draw.rect(surface, color, rect, width=0, border_radius=0, border_top_left_radius=-1, border_top_right_radius=-1, border_bottom_left_radius=-1, border_bottom_right_radius=-1)
+  #Drawing part -------------------------------------------------------------------------------
+  pygame.draw.rect(screen, Red, (cord_x, cord_y, 80, 80)) #<---Here is the 2 variables: Cord_x and Cord_y
+  #Create rectangle: pygame.draw.rect(superficie, color, (x position, y position, large, width)
   
+  #End drawing part ---------------------------------------------------------------------------
 
-  #TERMINAR SALA DE DIBUJO
-
+  #Refresh screen and add FPS
   pygame.display.flip()
+  clock.tick(30) #<---Lo que va dentro del paréntesis son los FPS
