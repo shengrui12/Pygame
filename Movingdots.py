70092a7ed17e05f505c9278f2abbc391b8fc4c8e
shengrui12 <shengrui1309@gmail.com>
2026-02-08 14:16:58 +0100

    Create Movingdots.py to display random dots
    Initial implementation of a Pygame application that displays random dots on the screen.

---
+++ Movingdots.py
@@ -0,0 +1,48 @@
+#Import Pygame
+import pygame
+import sys
+import random
+pygame.init()
+
+#Def pantalla
+size = (800, 500)
+screen = pygame.display.set_mode(size)
+
+#Def FPS(Frames per second)
+clock = pygame.time.Clock()
+
+#Def RGB 
+Black = (0, 0, 0)
+White = (255, 255, 255)
+Red = (255, 0, 0)
+Green = (0, 255, 0)
+Blue = (0, 0, 255)
+
+#List
+coordinate_list = []
+for value in range(60):
+  x = random.randint(0, 800)
+  y = random.randint(0, 500)
+  coordinate_list.append([x, y])
+
+#Loop
+while True:
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      sys.exit()
+  
+  #Background color
+  screen.fill(Black)
+
+  #Drawing part -------------------------------------------------------------------------------
+  for coordinate in coordinate_list:
+    x = coordinate[0]
+    y = coordinate[1]
+    pygame.draw.circle(screen, Red, (x, y), 2)
+    #pygame.draw.circle(surface, color, (x, y), radius)
+
+  #End drawing part ---------------------------------------------------------------------------
+
+  #Refresh screen and add FPS
+  pygame.display.flip()
+  clock.tick(60) #<---60 is the FPS
